b1ea38b1dee0db2d7e3c47173c2df7426b866e72
Tahxin <satahsin6@gmail.com>
2026-07-28 01:34:39 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,159 @@
+// console.log("Hello JavaScript!");
+
+// console.log("My name is Alex.");
+
+// console.log("I am excited to become a Web Developer.");
+
+
+
+// let num1 = 25;
+
+// let num2 = 10;
+
+ 
+
+// console.log("Addition:", num1 + num2);
+
+// console.log("Subtraction:", num1 - num2);
+
+// console.log("Multiplication:", num1 * num2);
+
+// console.log("Division:", num1 / num2);
+
+// console.log("Modulus:", num1 % num2);
+
+
+
+
+// let a = 100;
+
+// let b = "Programming";
+
+// let c = true;
+
+// let d = false;
+
+// let e = 3.1416;
+
+ 
+
+// console.log("a:", typeof a);
+
+// console.log("b:", typeof b);
+
+// console.log("c:", typeof c);
+
+// console.log("d:", typeof d);
+
+// console.log("e:", typeof e);
+
+
+
+
+
+// let firstName = "Alex";
+
+// let First_Name = "Alex";
+
+// let user123 = 1;
+
+// let $price = 19.99;
+
+// let _total = 0;
+
+ 
+
+// console.log(firstName, First_Name, user123, $price, _total);
+
+
+
+
+
+
+
+// // let name = "Alex";
+
+// // let age = 20;
+
+// // let favoriteFood = "Pizza";
+
+// // let favoriteLanguage = "JavaScript";
+
+// // let lovesCoding = true;
+
+ 
+
+// console.log("Hello!");
+
+// console.log("My name is " + name + ".");
+
+// console.log("I am " + age + " years old.");
+
+// console.log("My favorite food is " + favoriteFood + ".");
+
+// console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+// console.log("I love coding:", lovesCoding);
+
+
+
+// // let myName = "Shahjalal Ahmed Nishat";
+// // let myAge = 20;
+// // let myCountry = "Bangladesh";
+// // let myHobby = "Coding";
+
+
+// console.log("My name is " + myName + ".");
+// console.log("I am " + myAge + " years old.");
+// console.log("I am from " + myCountry + ".");
+// console.log("My hobby is " + myHobby + ".");
+
+
+
+
+
+
+
+
+
+let fullName = "Shahjalal Ahmed Nishat";
+
+let age = 26;
+
+let country = "Bangladesh";
+
+let favoriteLanguage = "TypeScript";
+
+let dreamJob = "Full-Stack Developer";
+
+let yearsToGoal = 1;
+
+let isLearningJS = true;
+
+ 
+
+console.log("========================");
+
+console.log("     MY PROFILE");
+
+console.log("========================");
+
+console.log();
+
+console.log("Name:", fullName);
+
+console.log("Age:", age);
+
+console.log("Country:", country);
+
+console.log("Favorite Language:", favoriteLanguage);
+
+console.log("Dream Job:", dreamJob);
+
+console.log("Years to Reach Goal:", yearsToGoal);
+
+console.log("Learning JavaScript:", isLearningJS);
+
+console.log();
+
+console.log("========================");
