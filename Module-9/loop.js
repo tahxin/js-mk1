199422e012353d33c31af76ebfab98b92ab17b58
Tahxin <satahsin6@gmail.com>
2026-07-28 03:24:35 +0600

    m8 upto problem 5

---
+++ Module-9/loop.js
@@ -0,0 +1,35 @@
+// for (let i = 0; i < 5; i++) {
+//     console.log("Hello");
+// }   
+
+
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log( i );
+// }
+
+// let i = 10;
+
+// while (i >= 1) {
+//     console.log(i);
+    
+//     i-- ;
+// }
+
+
+
+
+
+
+
+
+let sentence = "Loop Cholche";
+
+let i = 0
+
+while (i < 7) {
+    console.log(sentence);
+    i++;
+}
