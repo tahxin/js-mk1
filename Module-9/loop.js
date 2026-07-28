caa71ffabad87364ead120f06fdfdd82591645ca
Tahxin <satahsin6@gmail.com>
2026-07-28 11:01:14 +0600

    midway module 9

--- Module-9/loop.js
+++ Module-9/loop.js
@@ -25,11 +25,160 @@
 
 
 
-let sentence = "Loop Cholche";
+// let sentence = "Loop Cholche";
 
-let i = 0
+// let i = 0
 
-while (i < 7) {
-    console.log(sentence);
-    i++;
+// while (i < 7) {
+//     console.log(sentence);
+//     i++;
+// }
+
+
+
+
+// for loop even numbers 1-30
+
+
+// for (let i = 1; i <= 30; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+
+
+// sum of 1-20 for loop
+
+
+// let sum = 0;
+
+// for (let i = 1; i <= 20; i++) { 
+//     sum += i; 
+// }
+
+// console.log(sum);
+
+
+
+
+
+
+// let number = 5;
+
+// for (let i = 1; i <= 10; i++) {
+//     let result = number * i;
+//     console.log(`${number} x ${i} = ${result}`);
+// }
+
+
+
+
+
+
+
+// let number = 20;
+
+// for (let i = 1; i <= 20; i++) {
+//     console.log(number);
+//     number--;
+// }
+
+
+
+
+
+
+
+
+// let sum = 0;
+
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 2 === 0) {
+//         sum += i;
+//     }   
+// }
+
+// console.log(sum);
+
+
+
+// for (let i = 1; i <= 50; i++) {
+//     if (i > 30) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 4 === 0) {
+//         continue;   
+//     }  
+//     console.log(i);
+// } 
+
+
+
+// let i = 7;
+
+// while (i<=5) {
+    
+//     console.log(i);
+//     i++;
+// }
+
+
+
+
+
+// // for loop version
+
+
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
+ 
+ 
+// // while loop version
+
+// let i = 1;
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+
+ 
+// // do while loop version
+
+
+// let i =1 ;
+
+
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 5);
+
+
+
+
+
+
+
+
+for (let i = 1; i <= 100; i++) {
+   
+    if (i% 5===0) {
+        continue;
+    }  
+    else if (i > 40) {
+        break;
+    } 
+    console.log(i);
 }
